9425578e3860ba6a9dd56eda9b26befb5b95f1af
dmaniaa <danielmania93@gmail.com>
2025-10-25 10:08:38 +0200

    added the ability to deploy on Docker

--- main.py
+++ main.py
@@ -136,4 +136,6 @@ def upload_file():
         return render_template("result.html", entries=analyze_log_file("uploaded_log.log"))
     return "No file uploaded."
 
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port, debug=True)
