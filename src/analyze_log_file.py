a689d8c044f88191cb6e4f7ae8a8bd17de19552b
dmaniaa <danielmania93@gmail.com>
2025-10-25 11:49:53 +0200

    restructured, removed obsolete code, implemented unit test

---
+++ src/analyze_log_file.py
@@ -0,0 +1,112 @@
+import datetime
+import os
+from enum import Enum
+
+class EventCategory(Enum):
+    BRUTEFORCE = "Bruteforce"
+    SQL_INJECTION = "SQL Injection"
+    UNUSUAL_ACCESS = "Unusual Access"
+    PORT_SCAN = "Port Scan"
+
+#bazowa klasa wpisu logu - przed analizą i określeniem kategorii
+class LogEntry:
+    def __init__(self, timestamp, level, source, event, message):
+        self.timestamp = datetime.datetime.strptime(timestamp, "[%Y-%m-%d %H:%M:%S]")
+        self.level = level
+        self.source = source
+        self.event = event
+        self.message = message
+        self.category = None
+    
+ #klasy pochodne dla konkretnych kategorii zdarzeń   
+class BruteForceEvent(LogEntry):
+    def __init__(self, log_entry):
+        self.timestamp = log_entry.timestamp
+        self.level = log_entry.level
+        self.source = log_entry.source
+        self.username = log_entry.message.split("user=")[1]
+
+class SQLInjectionEvent(LogEntry):
+    def __init__(self, log_entry):
+        self.timestamp = log_entry.timestamp
+        self.source = log_entry.source
+        self.user_input = log_entry.message.split("user_input=")[1]
+    
+class UnusualAccessEvent(LogEntry):
+    def __init__(self, log_entry):
+        self.timestamp = log_entry.timestamp
+        self.source = log_entry.source
+        if log_entry.event == "UNUSUAL_ACCESS":
+            self.path = log_entry.message
+            self.code = None
+            self.method = None
+        if log_entry.event == "GET" or log_entry.event == "POST":
+            self.path = log_entry.message.split(" ")[0]
+            self.code = log_entry.message.split(" ")[1]
+            self.method = log_entry.event
+
+class PortScanEvent(LogEntry):
+    def __init__(self, log_entry):
+        self.timestamp = log_entry.timestamp
+        self.source = log_entry.source
+        self.port = log_entry.message.split("target=")[1]
+
+
+def analyze_log_file(filename):
+    log_entries = []
+    #odczyt i parsowanie wpisów do klasy
+    try:
+        with open(filename, "r") as file:
+            logfile_lines = file.readlines()
+            for line in logfile_lines:
+                line.strip()
+                timestamp = line.split()[0] + " " + line.split()[1]
+                level = line.split()[2]
+                source = line.split()[3]
+                event = line.split()[4]
+                message = " ".join(line.split()[5:])
+                log_entry = LogEntry(timestamp, level, source, event, message)
+                log_entries.append(log_entry)
+        os.remove(filename)
+    except:
+        return None
+
+    interesting_entries = []
+
+    #oznaczenie wpisów z kodami 401, 403 oraz typami wskazującymi na atak lub poziomem powyzej INFO
+    interesting_codes = ["401", "403"]
+    interesting_events = ["FAILED_LOGIN", "SQL_INJECTION_ATTEMPT", "UNUSUAL_ACCESS", "PORT_SCAN_ATTEMPT"]
+
+    for entry in log_entries:
+        if any(code in entry.message for code in interesting_codes):
+            interesting_entries.append(entry)
+        elif entry.event in interesting_events:
+            interesting_entries.append(entry)
+        elif entry.level in ["ERROR", "WARNING"]:
+            interesting_entries.append(entry)
+
+    failed_logins = []
+    bruteforce_entries = []
+    sql_injection_entries = []
+    unusual_access_entries = []
+    port_scan_entries = []
+
+    #sortowanie wpisów według rodzaju do odpowiednich klas
+    for entry in interesting_entries:
+        if entry.event == "FAILED_LOGIN":
+            failed_logins.append(entry)
+            if len(failed_logins) >= 2 and (failed_logins[-1].timestamp - failed_logins[-2].timestamp).seconds <= 3 and failed_logins[-1].source == failed_logins[-2].source:
+                bruteforce_entries.append(BruteForceEvent(entry))
+                failed_logins.clear()
+        if entry.event == "SQL_INJECTION_ATTEMPT":
+            sql_injection_entries.append(SQLInjectionEvent(entry))
+        if entry.event == "UNUSUAL_ACCESS":
+            unusual_access_entries.append(UnusualAccessEvent(entry))
+        if entry.event == "PORT_SCAN_ATTEMPT":
+            port_scan_entries.append(PortScanEvent(entry))
+        if any(code in entry.message for code in interesting_codes):
+            unusual_access_entries.append(UnusualAccessEvent(entry))
+        else:
+            continue
+
+    return [bruteforce_entries, sql_injection_entries, unusual_access_entries, port_scan_entries]
---
+++ tests/test_analyze_log_file.py
@@ -0,0 +1,47 @@
+import datetime
+from src.analyze_log_file import analyze_log_file, LogEntry, BruteForceEvent, SQLInjectionEvent, UnusualAccessEvent, PortScanEvent
+
+def test_analyze_log_file():
+    try:
+        with open("test_log.log", "w") as f:
+            f.write("[2025-10-25 10:50:00] WARNING 10.0.0.1 FAILED_LOGIN user=admin\n")
+            f.write("[2025-10-25 10:50:01] WARNING 10.0.0.1 FAILED_LOGIN user=admin\n")
+            f.write("[2025-10-25 10:50:02] WARNING 10.0.0.1 FAILED_LOGIN user=admin\n")
+            f.write("[2025-10-25 11:00:00] ERROR 10.0.10.5 SQL_INJECTION_ATTEMPT user_input=' OR '1'='1\n")
+            f.write("[2025-10-25 11:05:00] ERROR 10.5.10.5 UNUSUAL_ACCESS /etc/passwd\n")
+            f.write("[2025-10-25 11:05:01] INFO 10.5.10.5 GET /admin 403\n")
+            f.write("[2025-10-25 11:10:00] WARNING 192.168.0.50 PORT_SCAN_ATTEMPT target=22\n")
+    except:
+        assert False, "Failed to create test log file."
+
+    result = analyze_log_file("test_log.log")
+    assert result is not None
+    assert len(result) == 4  
+    assert len(result[0]) == 1
+    assert len(result[1]) == 1
+    assert len(result[2]) == 2
+    assert len(result[3]) == 1
+    assert isinstance(result[0][0], BruteForceEvent)
+    assert isinstance(result[1][0], SQLInjectionEvent)
+    assert isinstance(result[2][0], UnusualAccessEvent)
+    assert isinstance(result[2][1], UnusualAccessEvent)
+    assert isinstance(result[3][0], PortScanEvent)
+    assert result[0][0].timestamp == datetime.datetime(2025, 10, 25, 10, 50, 1)
+    assert result[0][0].source == "10.0.0.1"
+    assert result[0][0].username == "admin"
+    assert result[1][0].timestamp == datetime.datetime(2025, 10, 25, 11, 0, 0)
+    assert result[1][0].source == "10.0.10.5"
+    assert result[1][0].user_input == "' OR '1'='1"
+    assert result[2][0].timestamp == datetime.datetime(2025, 10, 25, 11, 5, 0)
+    assert result[2][0].source == "10.5.10.5"
+    assert result[2][0].path == "/etc/passwd"
+    assert result[2][0].code is None
+    assert result[2][0].method is None
+    assert result[2][1].timestamp == datetime.datetime(2025, 10, 25, 11, 5, 1)
+    assert result[2][1].source == "10.5.10.5"
+    assert result[2][1].path == "/admin"
+    assert result[2][1].code == "403"
+    assert result[2][1].method == "GET"
+    assert result[3][0].timestamp == datetime.datetime(2025, 10, 25, 11, 10, 0)
+    assert result[3][0].source == "192.168.0.50"
+    assert result[3][0].port == "22"
